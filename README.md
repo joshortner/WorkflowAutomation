6dba04caa5d8901f42ee34a7ff1fe05406bc89b5
Josh <jlortner@gmail.com>
2022-01-01 21:11:01 -0600

    init, and build first iteration done

--- README.md
+++ README.md
@@ -3,3 +3,40 @@
 This package contains scripts to automate some of the things I find myself doing often.
 
 ## cmake-man
+
+cmake-man is a CLI program to manage CMake projects without having to hand-edit a CMakeLists. Its based off of my personal CMake Project setup which is described below.
+
+### Installation
+
+Use the package manager pip to install cmake-man
+
+```bash
+pip install -i https://test.pypi.org/simple/ workflow-automation-pkg-joshortner
+```
+
+### Usage
+
+Create a CMake Project
+
+```bash
+mkdir <project_name>
+cd <project_name>
+cmake-man --init --lang c
+```
+
+Project Structure:
+
+|---<project_name>
+	|--- CMakeLists.txt
+	|--- lib
+	|--- src
+		|--- main.<lang>
+		|--- <project_name>.h
+		|--- <project_name>
+
+
+Build a CMake Project. This will add a "build" directory and generate files for the systems default compilation tools.
+
+```bash
+cmake-man --build
+```
--- src/workflow_automation/cmake_man.py
+++ src/workflow_automation/cmake_man.py
@@ -8,35 +8,56 @@ Source:
 # TO DO: --build option
 
 import argparse
+import shutil
+import subprocess
 from pathlib import Path
 
 
 def main():
     parser = argparse.ArgumentParser(description="Initialize a CMake project directory")
-    parser.add_argument("--lang", type=str, required=True, help="Project language (options: c, cpp)")
-    parser.add_argument("--dir", type=str, required=True, help="Project directory")
 
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-i", "--init", action="store_true", help="Initialize a directory with a CMake project template")
+    group.add_argument("-b", "--build", action="store_true", help="Build an existing CMake project")
+
+    parser.add_argument("-l", "--lang", type=str, help="Project language (options: c, cpp)")
+    
     args = parser.parse_args()
 
-    initializeCMakeProject(args.dir, args.lang)
+    if args.init:
+        initProject(Path.cwd(), args.lang)
+    elif args.build:
+        buildProject(Path.cwd())
 
 
+def buildProject(directory: Path):
+    # Check for CMakeLists file
+    check_file = directory.joinpath("CMakeLists.txt")
+    if check_file.is_file():
+        # Check for existing build path
+        build_path = directory.joinpath("build")
+        if build_path.is_dir():
+            shutil.rmtree(build_path)
 
-def initializeCMakeProject(directory: str, language: str):
-    directory = Path(directory)
-    if language in SUPPORTED_LANGUAGES:
-        if not directory.is_dir():
+        subprocess.call("mkdir build", shell=True)
+        subprocess.run("cd build && cmake ..", shell=True)
+
+    else:
+        print(f'Directory {directory} doesn\'t contain a CMakeLists.txt!')
+
+
+def initProject(directory: Path, language: str):
+    if language in LANGUAGE_FILES.keys():
+        # Check for existing CMakeLists file
+        check_file = directory.joinpath("CMakeLists.txt")
+        if not check_file.is_file():
             project_name = directory.name.lower()
-
-            directory.mkdir()
 
             lib_dir = directory.joinpath("lib")
             src_dir = directory.joinpath("src")
             src_project_dir = src_dir.joinpath(project_name)
-            default_module_dir = src_project_dir.joinpath("default")
 
             cmake_lists_path = directory.joinpath("CMakeLists.txt")
-            #build_script     = directory.joinpath("run_build.sh")
 
             entry_point_path  = src_dir.joinpath(f'main.{language}')
             main_include_path = src_dir.joinpath(f'{project_name}.h')
@@ -45,20 +66,19 @@ def initializeCMakeProject(directory: str, language: str):
             lib_dir.mkdir()
             src_dir.mkdir()
             src_project_dir.mkdir()
-            default_module_dir.mkdir()
 
             # Write default files
             with cmake_lists_path.open("w") as f:
-                f.write(cmake_lists_contents(project_name, language))
+                f.write(LANGUAGE_FILES[language]["cmake_lists"](project_name, language)) 
 
             with entry_point_path.open("w") as f:
-                f.write(main_c_contents(project_name))
+                f.write(LANGUAGE_FILES[language]["main"](project_name))
 
             with main_include_path.open("w") as f:
-                f.write(main_include_c_contents(project_name.upper()))
+                f.write(LANGUAGE_FILES[language]["main_include"](project_name))
 
         else:
-            print(f'Directory {directory} already exisits!')
+            print(f'Directory {directory} already contains a CMakeLists.txt!')
             return
 
     else:
@@ -66,39 +86,43 @@ def initializeCMakeProject(directory: str, language: str):
         return
 
 
-SUPPORTED_LANGUAGES = [
-    "c", "cpp"
-]
+LANGUAGE_FILES = {
+    "c": {
+        "cmake_lists": lambda project_name, language: \
+            (
+                f'cmake_minimum_required(VERSION 3.7.1)\n' +
+                f'set(CMAKE_C_STANDARD 99)\n\n' +
+                f'project({project_name})\n\n' +
+                f'set(SRC_DIR ${{CMAKE_CURRENT_SOURCE_DIR}}/src)\n\n' +
+                f'include_directories(\n\t${{SRC_DIR}}\n)\n\n' +
+                f'file(\n\tGLOB SOURCES\n\t${{SRC_DIR}}/main.{language}\n)\n\n' +
+                f'add_executable(\n\t${{PROJECT_NAME}}\n\t${{SOURCES}}\n)\n\n' +
+                f'#target_link_libraries(\n#\t${{PROJECT_NAME}}\n#\t<lib>\n#)'
+            ),
 
-cmake_lists_contents = lambda project_name, language: \
-    (
-        f'cmake_minimum_required(VERSION 3.7.1)\n' +
-        f'set(CMAKE_C_STANDARD 99)\n\n' +
-        f'project({project_name})\n\n' +
-        f'set(SRC_DIR ${{CMAKE_CURRENT_SOURCE_DIR}}/src)\n\n' +
-        f'include_directories(\n\t${{SRC_DIR}}\n)\n\n' +
-        f'file(\n\tGLOB SOURCES\n\t${{SRC_DIR}}/main.{language}\n)\n\n' +
-        f'add_executable(\n\t${{PROJECT_NAME}}\n\t${{SOURCES}}\n)\n\n' +
-        f'#target_link_libraries(\n#\t${{PROJECT_NAME}}\n#\t<lib>\n#)'
-    )
+        "main": lambda project_name: \
+            (
+                f'#include <{project_name}.h>\n\n' +
+                "#include <stdio.h>\n" +
+                f'int main(int argc, char **argv)\n' +
+                "{\n" +
+                '\tprintf("Hello World\\n");\n' +
+                f'\treturn 0;\n'
+                "}\n"
+            ),
 
-main_c_contents = lambda project_name: \
-    (
-        f'#include <{project_name}.h>\n\n' +
-        "#include <stdio.h>\n" +
-        f'int main(int argc, char **argv)\n' +
-        "{\n" +
-        '\tprintf("Hello World\\n");\n' +
-        f'\treturn 0;\n'
-        "}\n"
-    )
+        "main_include": lambda project_name: \
+            (
+                f'#ifndef {project_name}_H\n' +
+                f'#define {project_name}_H\n\n' +
+                f'#endif'
+            )
+    }
+}
 
-main_include_c_contents = lambda project_name: \
-    (
-        f'#ifndef {project_name}_H\n' +
-        f'#define {project_name}_H\n\n' +
-        f'#endif'
-    )
+#main_c_contents = lambda project_name: \
+
+#main_include_c_contents = lambda project_name: \
 
 if __name__ == "__main__":
     main()
